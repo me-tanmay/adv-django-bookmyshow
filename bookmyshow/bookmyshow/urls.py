b86ac112e48aede9358f04a0ca459e7dc0d487d6
me-tanmay <tanchakraborty@deloitte.com>
2024-09-20 14:09:55 +0530

    modified folder structure

--- bookmyshow/bookmyshow/urls.py
+++ bookmyshow/bookmyshow/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from bookmyshow_api import urls as bookmyshow_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('bookmyshow/', include(bookmyshow_urls))
 ]
