b0f313fa70fec48e9b3579162c5bda8e3539acb1
me-tanmay <tanchakraborty@deloitte.com>
2024-09-24 21:01:57 +0530

    Event, Booking and Payments Model

--- bookmyshow/bookmyshow_api/models.py
+++ bookmyshow/bookmyshow_api/models.py
@@ -12,3 +12,32 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='events')
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='bookings')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='bookings')
+    booking_date = models.DateTimeField(auto_now_add=True)
+    number_of_tickets = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.email} - {self.event.name}"
+
+class Payment(models.Model):
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE, related_name='payment')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Payment for {self.booking.event.name} by {self.booking.user.email}"
